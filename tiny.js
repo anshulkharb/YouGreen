2a52971b9f50322c6af7a7e19211af17c56f37c6
anshulkharb <help.geniustiger@gmail.com>
2019-09-23 21:28:12 +0530

    YouGreen new changes

--- tiny.js
+++ tiny.js
@@ -15,7 +15,7 @@ localStorage['yt-player-quality'] = JSON.stringify(saved_q);
 chrome.runtime.onMessage.addListener( function(request, sender) {
     var x = document.getElementById("date")
     var dateText = x.innerText.slice(1, 27)
-    x.innerText = " • " + dateText + " • Congrats, you've saved upto"+ request.CO2_saved +"kg of C02 • "
+    x.innerText = " • " + dateText + " • Congrats, you've saved upto "+ request.CO2_saved +"kg of C02 • "
     var helpLink = document.createElement('a');
     helpLink.setAttribute('href',"https://anshulkharb.com/side_projects/yougreen");
     helpLink.setAttribute('target',"_blank");
